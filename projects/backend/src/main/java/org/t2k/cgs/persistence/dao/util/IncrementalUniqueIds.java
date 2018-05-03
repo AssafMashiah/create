29136eb9bd0dae3e460cc8ffa2f65781ba39ef09
Alex Burdusel <alexandru.burdusel@gmail.com>
2018-05-03 18:20:08 +0300

    Adds TestNG suite file and fixes incremental ids being removed on tests
    Former-commit-id: 981dd17916068430b655a46af0ac41c72c38c10e

--- projects/backend/src/main/java/org/t2k/cgs/persistence/dao/util/IncrementalUniqueIds.java
+++ projects/backend/src/main/java/org/t2k/cgs/persistence/dao/util/IncrementalUniqueIds.java
@@ -4,7 +4,6 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
-import org.springframework.stereotype.Service;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,22 +11,25 @@ import org.springframework.stereotype.Service;
  * Date: 24/06/13
  * Time: 15:19
  */
-@Service
-public  class IncrementalUniqueIds {
-    static final String INCREMENTAL_IDS_COLLECTION_NAME = "incrementals";
-    static final String ID_FIELD_NAME = "incrementalId";
-    static final String COLLECTION_NAME_FIELD_NAME = "collectionName";
+public class IncrementalUniqueIds {
+    private static final String INCREMENTAL_IDS_COLLECTION_NAME = "incrementals";
+    private static final String ID_FIELD_NAME = "incrementalId";
+    private static final String COLLECTION_NAME_FIELD_NAME = "collectionName";
 
     public synchronized static int getNextId(DB db, String collectionName) {
-
         DBCollection seq = db.getCollection(INCREMENTAL_IDS_COLLECTION_NAME);
         DBObject query = new BasicDBObject();
         query.put(COLLECTION_NAME_FIELD_NAME, collectionName);
-
         DBObject change = new BasicDBObject(ID_FIELD_NAME, 1);
         DBObject update = new BasicDBObject("$inc", change);
-
-        DBObject res = seq.findAndModify(query, new BasicDBObject(), new BasicDBObject(), false, update, true, true);
-        return ((Integer) res.get(ID_FIELD_NAME)).intValue();
+        DBObject res = seq.findAndModify(
+                query,
+                new BasicDBObject(),
+                new BasicDBObject(),
+                false,
+                update,
+                true,
+                true);
+        return (Integer) res.get(ID_FIELD_NAME);
     }
 }
--- projects/backend/src/test/java/org/t2k/cgs/persistence/dao/util/UniqueIdDaoTest.java
+++ projects/backend/src/test/java/org/t2k/cgs/persistence/dao/util/UniqueIdDaoTest.java
@@ -1,13 +1,9 @@
 package org.t2k.cgs.persistence.dao.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.t2k.cgs.Application;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,25 +12,27 @@ import static org.testng.Assert.assertEquals;
  * Time: 2:40 PM
  */
 @SpringApplicationConfiguration(classes = Application.class)
+@Test(groups = "ignore")
+// fixme this class removes all the incrementals, causing loss of current incrementalId on the database
 public class UniqueIdDaoTest extends AbstractTestNGSpringContextTests {
 
-    @Autowired
-    UniqueIdMongoDao uniqueIdDao;
-
-    @BeforeMethod
-    private void reset() throws Exception {
-        uniqueIdDao.reset();
-    }
-
-    @Test
-    private void testIdGeneration() throws Exception {
-
-        assertEquals(uniqueIdDao.getNextId("bbb"), 1);
-        assertEquals(uniqueIdDao.getNextId("ccc"), 1);
-        assertEquals(uniqueIdDao.getNextId("ddd"), 1);
-        assertEquals(uniqueIdDao.getNextId("bbb"), 2);
-        assertEquals(uniqueIdDao.getNextId("bbb"), 3);
-        assertEquals(uniqueIdDao.getNextId("ccc"), 2);
-    }
+//    @Autowired
+//    UniqueIdMongoDao uniqueIdDao;
+//
+//    @BeforeMethod
+//    private void reset() throws Exception {
+//        uniqueIdDao.reset();
+//    }
+//
+//    @Test
+//    private void testIdGeneration() throws Exception {
+//
+//        assertEquals(uniqueIdDao.getNextId("bbb"), 1);
+//        assertEquals(uniqueIdDao.getNextId("ccc"), 1);
+//        assertEquals(uniqueIdDao.getNextId("ddd"), 1);
+//        assertEquals(uniqueIdDao.getNextId("bbb"), 2);
+//        assertEquals(uniqueIdDao.getNextId("bbb"), 3);
+//        assertEquals(uniqueIdDao.getNextId("ccc"), 2);
+//    }
 
 }
